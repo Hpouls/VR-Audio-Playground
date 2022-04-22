8bc06c3b94549a6d0dd8308909b6c1beb2eb6e8b
Hpouls <Hpouls20@student.aau.dk>
2022-04-22 13:45:11 +0200

    Used assets changed height

--- -ProjectName-/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ -ProjectName-/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,23 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID TELEPHONEBELL_PLAYER = 2607810968U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 2385628198U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID STONE = 1216965916U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS
+
+    } // namespace SWITCHES
 
     namespace BANKS
     {
